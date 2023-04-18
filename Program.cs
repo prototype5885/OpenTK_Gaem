a4f8997b83a613eb7db1b451bad3afe792d301a4
prototype5885 <prototype5885@gmail.com>
2023-04-18 02:15:14 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace LearnOpenTK
 
         float[] vertices =
         {
-            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            -1.0f, -0.5f, 0.0f, //Bottom-left vertex
             0.5f, -0.5f, 0.0f, //Bottom-right vertex
             0.0f,  0.5f, 0.0f  //Top vertex
         };
