746e313953e013cbe9d5c99bdb843dbd5eb0afcc
prototype5885 <prototype5885@gmail.com>
2023-04-18 02:13:09 +0200

    XD

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Reflection.Metadata;
+
+namespace LearnOpenTK
+{
+    public class Game : GameWindow
+    {
+        int VertexBufferObject;
+        Shader shader;
+        int VertexArrayObject;
+
+
+        float[] vertices =
+        {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f, //Bottom-right vertex
+            0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+
+
+
+        protected override void OnLoad()
+        {
+            
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            shader = new Shader("shader.vert", "shader.frag");
+            VertexBufferObject = GL.GenBuffer();
+            VertexArrayObject = GL.GenVertexArray();
+
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+
+
+
+            base.OnLoad();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            shader.Use();
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
+
+            base.OnRenderFrame(e);
+        }
+
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+            shader.Dispose();
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(1600, 1200, "LearnOpenTK"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
